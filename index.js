b605aae9724efe8827183b1fbed97e455f6f6d1e
lgm527 <laurellmccaffrey@gmail.com>
2019-08-14 17:20:12 -0400

    map and reduce callbacks

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Add your functions here
+function map(array, fn){
+  let result = [];
+  for (var i = 0; i < array.length; i++) {
+    result.push(fn(array[i]));
+  }
+  return result;
+}
+
+function reduce(array, fn, start){
+  let total = (!!start) ? start : array[0];
+  let i = (!!start) ? 0 : 1;
+  for (; i < array.length; i++) {
+    total = fn(array[i], total);
+  }
+  return total;
+}
